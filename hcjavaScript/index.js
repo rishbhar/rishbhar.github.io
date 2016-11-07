6f4241d68461cf1e02f535b9412143a7d84af628
Rishabh Bhardwaj <rishbhar@indiana.edu>
2016-11-07 01:15:08 -0500

    move changes related to Shelby Work
    move changes related to Shelby Work

---
+++ hcjavaScript/index.js
@@ -0,0 +1,134 @@
+$(function () {
+
+
+
+
+
+/*$("#fsModalAssistiveTech").on("show", function () {
+  $("body").addClass("when-modal-open");
+alert("zsajsdakjsdh");
+  $("#fsModalUBER").modal('toggle');
+
+}).on("hidden", function () {
+  $("body").removeClass("when-modal-open")
+});
+
+
+$("#fsModalUBER").on("show", function () {
+  $("body").addClass("when-modal-open");
+alert("sds");
+  $("#fsModalAssistiveTech").modal('toggle');
+
+}).on("hidden", function () {
+  $("body").removeClass("when-modal-open")
+});
+
+
+/*ends*/
+
+//jQuery to collapse the navbar on scroll
+/*$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});*/
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top-90/*70 added by Rishabh*/
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+//nav bar shrink
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 150) {
+    //$('nav').addClass('shrink');
+    //$('#navLinksBar').addClass('shrinkNavLinkBar');
+    $('#menuTab').addClass('fixedTop');
+    $('#menuTab').removeClass('relativePosition');
+   // $('#logoImage').attr("src","LOGO_Block.png");
+    //  $('#collapseButton').css("margin-top","20px");
+   
+  } else {
+    //$('nav').removeClass('shrink');
+    //$('#navLinksBar').removeClass('shrinkNavLinkBar');
+      $('#menuTab').removeClass('fixedTop');
+    $('#menuTab').addClass('relativePosition');
+   // $('#logoImage').attr("src","LOGO_Block.jpg");
+   // $('#collapseButton').css("margin-top","48px");
+  }
+});
+
+
+
+
+
+
+$('.carousel-control.left').click(function() {
+  $('#myCarousel').carousel('prev');
+});
+
+$('.carousel-control.right').click(function() {
+  $('#myCarousel').carousel('next');
+});
+
+
+$('a[data-slide="prev"]').click(function() {
+  $('#myCarousel').carousel('prev');
+});
+
+$('a[data-slide="next"]').click(function() {
+  $('#myCarousel').carousel('next');
+});
+
+
+$("[id^='workExp']").click(
+	function()
+	{
+		//alert(this.id.search("ContentBlock"));
+		if(-1!=(this.id.search("ContentBlock")))
+			return false;
+		var currentPosition = $(window).scrollTop();
+		//alert("currentPosition"+currentPosition);
+		var secondNavBarPosition = $('#navLinks').offset().top;
+		//alert("secondNavBarPosition"+secondNavBarPosition);
+
+		//$('[id^="ContentBlock"]').css("display","none");
+		$("[id*=ContentBlock]").css("display","none");
+		var id=this.id+'ContentBlock';
+		//alert(id);
+		$('#'+id).css("display","block");
+		if(currentPosition > secondNavBarPosition)
+		$(window).scrollTop($('#navLinks').offset().top);
+		
+	});
+
+
+/*Code for making modal open and close*/
+
+$('#fsModalAssistiveTech').on('shown.bs.modal', function() {
+    $("#fsModalUBER").modal('hide');
+});
+
+$('#fsModalUBER').on('shown.bs.modal', function() {
+    $("#fsModalAssistiveTech").modal('hide');
+});
+
+
+//$('#myPic').height($('#aboutText').height());
+//alert($('#myPic').height());
+
+
+
+//$("#fsModalForums").modal('show');
+
+
+});
